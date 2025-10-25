4ab06b238ed830c657b8798b574fad7a37abf0b1
voyadger01 <voyadgerkx@gmail.com>
2025-10-25 14:25:33 +0300

    looks good but wrong

--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 
-int main()
-{
+int main(){
   using u_t = unsigned;
   u_t a = 0;
   std::cin >> a;
+
+  if (std::cin.eof()){
+    std::cout << 1;
+    std::cout << '\n';
+  }
+  else if (std::cin.fail()){
+    std::cerr << "Input error\n";
+    return 1;
+  }
 }
