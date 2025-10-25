df636cb232b806275e995c5bcb7a65642a185b34
voyadger01 <voyadgerkx@gmail.com>
2025-10-25 14:55:16 +0300

    Pyth add but dont work

--- main.cpp
+++ main.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 
+bool isPyth(unsigned a, unsigned b, unsigned c){
+  bool p = a == b*b + c*c;
+  p = p || b*b == a*a + c*c;
+  p = p || c*c == a*a + b*b;
+  return p;
+}
+
 int main(){
   using u_t = unsigned;
-  u_t a = 0;
+  u_t a = 0, b = 0, c = 0;
   size_t count = 0;
   while (std::cin >> a){
-    count += a*a;
+    count += isPyth(a,b,c) ? 1 : 0;
   }
   if (std::cin.eof()){
     std::cout << count;
