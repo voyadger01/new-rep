5f554678bc1d73f40e96171fa518d92586e20a85
voyadger01 <voyadgerkx@gmail.com>
2025-10-25 15:01:22 +0300

    pyth mb work

--- main.cpp
+++ main.cpp
@@ -10,9 +10,12 @@ bool isPyth(unsigned a, unsigned b, unsigned c){
 int main(){
   using u_t = unsigned;
   u_t a = 0, b = 0, c = 0;
+  std::cin >> c >> b;
   size_t count = 0;
   while (std::cin >> a){
     count += isPyth(a,b,c) ? 1 : 0;
+    c = b;
+    b = a;
   }
   if (std::cin.eof()){
     std::cout << count;
